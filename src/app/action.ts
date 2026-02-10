1404a2a6148c1b04fa11a0592a011bdaa3ee15e1
arturbchagas <arturbbcc@gmail.com>
2026-02-10 15:29:36 -0300

    fix: lint errors for deploy

--- src/app/action.ts
+++ src/app/action.ts
@@ -1,8 +1,9 @@
 import prisma from "@/lib/prisma";
 import { OrderStatus } from "@prisma/client";
 import { revalidatePath } from "next/cache";
+import { FormItem } from "@/types/Form-itens/FormItem";
 
-export async function creatServiceOrder(formData: any) {
+export async function createServiceOrder(formData: Omit<FormItem, 'id' | 'createdAt' | 'updatedAt'>) {
     await prisma.serviceOrder.create({ data: formData });
     revalidatePath("/");
 }
--- src/types/Form-itens/FormItem.ts
+++ src/types/Form-itens/FormItem.ts
@@ -1,16 +1,16 @@
-import { type OrderStatus } from "@prisma/client"
+import { OrderStatus } from "@prisma/client"
 export interface FormItem {
   id: string;
   name: string;
   phone?: string;
   email?: string;
   address?: string;
-  brand: string;
-  model: string;
+  brand?: string;
+  model?: string;
   serialNumber?: string;
   defects: string;
   defectsHistory?: string;
   status: OrderStatus;
   createdAt?: Date | string;
-  updateAt?: Date | string;
+  updatedAt?: Date | string;
 }
